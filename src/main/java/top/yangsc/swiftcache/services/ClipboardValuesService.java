8c0a4f7bac343bd5eb4776c1b8252a6e4b08139a
yang <y2055541461@gmail.com>
2025-05-13 11:27:39 +0800

    删除了几个不必要的类

--- src/main/java/top/yangsc/swiftcache/services/ClipboardValuesService.java
+++
@@ -1,7 +0,0 @@
-package top.yangsc.swiftcache.services;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import top.yangsc.swiftcache.base.pojo.ClipboardValues;
-
-public interface ClipboardValuesService extends IService<ClipboardValues> {
-}
--- src/main/java/top/yangsc/swiftcache/services/ValueTableService.java
+++
@@ -1,7 +0,0 @@
-package top.yangsc.swiftcache.services;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import top.yangsc.swiftcache.base.pojo.ValueTable;
-
-public interface ValueTableService extends IService<ValueTable> {
-}
